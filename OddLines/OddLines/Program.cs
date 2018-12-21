2cf7d054b91001071e5c62b46bd630699ea5e875
Christian Nytra <ch.nytra.13@gmail.com>
2018-12-21 11:22:07 +0100

    fertig

---
+++ OddLines/OddLines/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OddLines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> listOfLines = new List<string>();
+
+            using (StreamReader reader = new StreamReader(@"C:\text.txt"))
+            {
+                string line;
+
+                while ((line = reader.ReadLine()) != null)
+                {
+                    listOfLines.Add(line);
+                }
+            }
+
+            for (int i = 0; i < listOfLines.Count; i++)
+            {
+                if (i%2 > 0)
+                    Console.WriteLine(listOfLines[i]);
+                else
+                    continue;
+            }
+        }
+    }
+}
