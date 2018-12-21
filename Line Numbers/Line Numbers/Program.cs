4910bb5f51dd4cfce89f8f70b2b4e72e7e60613c
Christian Nytra <ch.nytra.13@gmail.com>
2018-12-21 11:43:49 +0100

    fertig

---
+++ Line Numbers/Line Numbers/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Line_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> listOfLines = new List<string>();
+
+            using (StreamReader reader = new StreamReader(@"C:\text.txt"))
+            {
+                string line;
+                int i = 1;
+
+                while ((line = reader.ReadLine()) != null)
+                {
+                    listOfLines.Add($"Line{i}:\t" + line);
+
+                    i++;
+                }
+            }
+
+            using (StreamWriter writer = new StreamWriter(@"C:\output.txt"))
+            {
+                foreach (var line in listOfLines)
+                {
+                    writer.WriteLine(line);
+                }
+            }
+
+            
+        }
+    }
+}
